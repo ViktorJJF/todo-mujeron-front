dd09cd12dc8da6db00f85eb77bf19503fde6d9cf
ViktorJJF <vj.jimenez96@gmail.com>
2021-09-23 18:37:19 -0500

    agregado verificacion estados whatsapp

--- src/services/api/whatsapp.js
+++ src/services/api/whatsapp.js
@@ -3,4 +3,10 @@ export default {
   restartWhatsapp() {
     return axios.post("https://todo-full.digital/api/restart-whatsapp");
   },
+  restartSoftWhatsapp() {
+    return axios.post("http://localhost:3000/api/restart-soft-whatsapp");
+  },
+  getStatus() {
+    return axios.post("http://localhost:3000/api/whatsapp/status");
+  },
 };
