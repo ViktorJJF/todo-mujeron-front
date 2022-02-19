69b395ed145ce864b849aec37d8fc9b45755e588
ViktorJJF <vj.jimenez96@gmail.com>
2022-02-19 11:06:42 -0500

    added transport to websocket

--- src/plugins/sockets.js
+++ src/plugins/sockets.js
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 
-const socket = io("https://todo-full.digital");
+const socket = io("https://todo-full.digital", { transports: ["websocket"] });
 const store = require("@/store/index.js");
 
 socket.on("NEW_MESSAGE", (data) => {
