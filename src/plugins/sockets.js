a80c587885d0a91d32b224608e0de60e8131b9d6
ViktorJJF <vj.jimenez96@gmail.com>
2022-02-19 10:38:45 -0500

    changed socket domain

---
+++ src/plugins/sockets.js
@@ -0,0 +1,33 @@
+import io from "socket.io-client";
+
+// ruta prod
+const socket = io("https://todo-full.digital");
+const store = require("@/store/index.js");
+
+socket.on("NEW_MESSAGE", (data) => {
+  //   console.log("los mensajes: ", store.default);
+  let chatId = store.default.getters["chatsModule/getChatById"](data.chatId);
+  let chatsModule = store.default.state.chatsModule;
+  let chats = chatsModule.chats;
+  let selectedChat = chatsModule.selectedChat;
+
+  // agregando mensaje a chat
+  if (selectedChat && data.chatId === selectedChat._id) {
+    store.default.commit("chatsModule/addMessage", {
+      text: data.text,
+      from: data.from,
+      type: "text",
+      chatId,
+    });
+  }
+  // actualizando ultima fecha mensaje recibido
+  chats.find(
+    (el) => el._id === data.chatId
+  ).last_message[0].createdAt = new Date();
+});
+
+socket.on("NEW_CHAT", (data) => {
+  store.default.commit("chatsModule/addChat", data);
+});
+
+export default socket;
