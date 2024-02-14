918805e279c3d98f10093180775b086d925e7fc8
Cristian Peralta <cbperaltasegura@gmail.com>
2024-02-14 10:49:05 -0500

    feat: Add company param to create a new cleanLead. Also add companies param to get cleanLead list

--- src/views/Leads.vue
+++ src/views/Leads.vue
@@ -1027,6 +1027,7 @@ export default {
         sort: "updatedAt",
         order: "desc",
       };
+      body["companies"] = [this.$store.getters["authModule/getCurrentCompany"].company._id];
       if (this.telefonoId) body["telefonoId"] = this.telefonoId._id;
       if (this.filterCountries.length > 0) body["pais"] = this.filterCountries;
       if (this.selectedLabels && this.selectedLabels.length > 0) {
@@ -1243,6 +1244,7 @@ export default {
           await this.$store.dispatch("cleanLeadsModule/create", {
             ...this.editedItem,
             is_manual: true,
+            company: this.$store.getters["authModule/getCurrentCompany"].company._id,
           });
           //refrescar tabla
           this.initialize(
