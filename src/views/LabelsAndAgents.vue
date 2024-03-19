3013a79257be570688edc4b1365874d2d881ee40
Cristian Peralta <cbperaltasegura@gmail.com>
2024-03-19 15:48:43 -0500

    feat: Add companies parameter to filter todofull labels by companies in LabelsAndAgents view

--- src/views/LabelsAndAgents.vue
+++ src/views/LabelsAndAgents.vue
@@ -135,7 +135,9 @@ export default {
         this.$store.dispatch("ecommercesTagsModule/list", {
           companies: [this.$store.getters["authModule/getCurrentCompany"].company._id],
         }),
-        this.$store.dispatch("todofullLabelsModule/list"),
+        this.$store.dispatch("todofullLabelsModule/list", {
+          companies: [this.$store.getters["authModule/getCurrentCompany"].company._id],
+        }),
         this.$store.dispatch("ecommercesAttributesModule/list", {
           companies: [this.$store.getters["authModule/getCurrentCompany"].company._id],
         }),
