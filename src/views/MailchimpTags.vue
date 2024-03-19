160fc79ffe57b569cbab03a8b721dcf1804218de
Cristian Peralta <cbperaltasegura@gmail.com>
2024-03-19 14:06:00 -0500

    feat: Add companies parameter to filter ecommerce tags by companies in MailchimpTags view

--- src/views/MailchimpTags.vue
+++ src/views/MailchimpTags.vue
@@ -358,7 +358,9 @@ export default {
           companies: [this.$store.getters["authModule/getCurrentCompany"].company._id],
         }),
         this.$store.dispatch("ecommercesCategoriesModule/list"),
-        this.$store.dispatch("ecommercesTagsModule/list"),
+        this.$store.dispatch("ecommercesTagsModule/list", {
+          companies: [this.$store.getters["authModule/getCurrentCompany"].company._id],
+        }),
         this.$store.dispatch("todofullLabelsModule/list"),
         this.$store.dispatch("ecommercesAttributesModule/list", {
           companies: [this.$store.getters["authModule/getCurrentCompany"].company._id],
