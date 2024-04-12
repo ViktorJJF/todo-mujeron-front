259c1e96786dcdedb61088e2f2600dffcc37a9e3
Cristian Peralta <cbperaltasegura@gmail.com>
2024-04-12 09:20:30 -0500

    feat: Remove pais filter from listAll service belonged to leads and cleanLeads services

--- src/services/api/cleanLeads.js
+++ src/services/api/cleanLeads.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-import store from "@/store";
 
 export default {
   list(query = { sort: "createdAt", order: "1" }) {
@@ -27,16 +26,6 @@ export default {
   },
   listAll(query = { sort: "createdAt", order: "1" }) {
     if (query.telefonoId === null) query.telefonoId = "";
-    if (query.pais) {
-      //filtro pais
-      let filterCountries = "";
-      for (const paisIndex of query.pais) {
-        filterCountries += "pais=" + store.state.countries[paisIndex] + "&";
-      }
-      return axios.get("/api/clean-leads/all?" + filterCountries, {
-        params: query,
-      });
-    }
     return axios.get("/api/clean-leads/all", { params: query });
   },
   listOne(id) {
