20c5ffe530d97cb5d08132a5473dbfd33a04d08b
Cristian Peralta <cbperaltasegura@gmail.com>
2024-05-02 11:23:51 -0500

    feat: Remove country selector and input. Add company parameter to create a new facebookAduience

--- src/views/FacebookAudiences.vue
+++ src/views/FacebookAudiences.vue
@@ -9,20 +9,6 @@
         text="Listado audiencias"
       >
         <v-col cols="12" sm="10">
-          <v-row class="mb-3">
-            <v-col cols="12" sm="6" md="6">
-              <v-select
-                color="primary"
-                prepend-icon="mdi-map"
-                dense
-                hide-details
-                outlined
-                :items="$store.state.countries"
-                v-model="country"
-                @change="initialize(country)"
-              ></v-select>
-            </v-col>
-          </v-row>
           <v-dialog v-model="dialog" max-width="600px">
             <template v-slot:activator="{ on }">
               <v-btn
@@ -58,18 +44,6 @@
                         v-model="adManagerId"
                         label="Coloca el nombre de la audiencia"
                       />
-                    </v-col>
-                    <v-col cols="12" sm="6" md="6">
-                      <div class="body-1 font-weight-bold">Pais</div>
-                      <v-select
-                        disabled
-                        dense
-                        hide-details
-                        placeholder="Seleccione un pais"
-                        outlined
-                        :items="['Peru', 'Colombia', 'Chile']"
-                        v-model="country"
-                      ></v-select>
                     </v-col>
                     <v-col cols="12" sm="12" md="12">
                       <span class="font-weight-bold">Nombre</span>
@@ -377,7 +351,7 @@ export default {
     return {
       showMissingLeads: false,
       currentView: null,
-      country: "Chile",
+      country: this.$store.getters["authModule/getCurrentCompany"].company.country,
       adManagerId: null,
       totalDocs: 0,
       cleanLeads: [],
@@ -447,7 +421,6 @@ export default {
     async initialize() {
       await Promise.all([
         this.$store.dispatch("facebookAudiencesModule/list", {
-          // country: country || this.country,
           sort: "name",
           order: "asc",
           companies: [this.$store.getters["authModule/getCurrentCompany"].company._id],
@@ -464,7 +437,7 @@ export default {
 
       // leyendo audiencias de api facebook
       let res = await axios.get("/api/graph-api/getAudiences", {
-        params: { country: this.country },
+        params: { company: this.$store.getters["authModule/getCurrentCompany"].company._id },
       });
       console.log("🚀 Aqui *** -> res", res);
       this.adManagerId = res.data.payload.ad_manager_id;
@@ -501,7 +474,6 @@ export default {
             id: itemId,
             data: {
               ...this.editedItem,
-              country: this.country,
               ad_manager_id: this.adManagerId,
             },
           });
@@ -513,6 +485,7 @@ export default {
       } else {
         //create item
         try {
+          this.editedItem.company = this.$store.getters["authModule/getCurrentCompany"].company._id;
           axios
             .post("/api/graph-api/audiences", { ...this.editedItem })
             .then(async (res) => {
@@ -521,14 +494,12 @@ export default {
                 "facebookAudiences" + "Module/create",
                 {
                   ...this.editedItem,
-                  country: this.country,
                   ad_manager_id: this.adManagerId,
                   external_id: res.data.payload.id,
                 }
               );
               this.audiences.unshift({
                 ...createdItem,
-                country: this.country,
                 ad_manager_id: this.adManagerId,
                 external_id: res.data.payload.id,
               });
@@ -632,9 +603,6 @@ export default {
         id: item._id,
         data: item,
       });
-    },
-    openForm() {
-      this.editItem.country = this.country;
     },
   },
 };
