1cd587fcfe650f3b0145acb995ed3d6103f8d0c9
Angel Gonzalez <angel.egb@outlook.com>
2024-05-30 18:03:34 -0400

    Add discount price to pdf on mobile

--- src/views/Catalog/mobile/index.vue
+++ src/views/Catalog/mobile/index.vue
@@ -765,23 +765,63 @@ export default {
         doc.addImage(image, "JPEG", x, y, width, height);
 
         if (price) {
-          let productPrice =
-            product.regular_price || product.variations[0].regular_price;
-          productPrice = new Intl.NumberFormat().format(productPrice);
-          let priceText = `Precio: ${productPrice}`;
+          const productPrice = product.regular_price || product.variations[0].regular_price
+          const salePrice = product.sale_price || product.variations[0].sale_price
+          
+          const priceText = `Precio: ${this.formatAmount(productPrice)}`
 
-          doc
-            .setFontSize(doc.getFontSize() + 2)
-            .setFont(undefined, "bold")
+          const priceTextPosition = {
+            x: width + x + 6,
+            y: height + y - rightText.length * 2.65
+          }
+
+          const hasDiscount = !!salePrice
+          if (hasDiscount) {
+            const priceTextWidth = doc.getTextWidth(priceText)
+            const discountText = `Precio: ${this.formatAmount(salePrice)}`
+
+            const priceLinePosition = {
+              x: priceTextPosition.x,
+              y: priceTextPosition.y - priceTextWidth
+            }
+            
+            doc
+            .setTextColor(204, 204, 204)
             .text(
               priceText,
-              width + x + 6,
-              height + y - rightText.length * 2.65,
+              priceTextPosition.x,
+              priceTextPosition.y,
               { angle: 90 }
-            );
+            )
+            .line(
+              priceTextPosition.x - 2,
+              priceTextPosition.y,
+              priceLinePosition.x - 2,
+              priceLinePosition.y
+            )
+            .setTextColor(0, 0, 0)
+            .setFont(undefined, 'bold')
+            .text(
+              discountText,
+              priceLinePosition.x,
+              priceLinePosition.y - 4,
+              { angle: 90 }
+            )
+
+          } else {
+            doc
+              .setFontSize(doc.getFontSize() + 2)
+              .setFont(undefined, 'bold')
+              .text(
+                priceText,
+                priceTextPosition.x,
+                priceTextPosition.y,
+                { angle: 90 }
+              )
+          }
 
           // return font to normal
-          doc.setFontSize(doc.getFontSize() - 2).setFont(undefined, "normal");
+          doc.setFontSize(doc.getFontSize() - 2).setFont(undefined, 'normal')
         }
 
         const filename = `${Date.now()}.pdf`;
@@ -837,6 +877,9 @@ export default {
     clearFilters() {
       this.filter.categories = [];
       this.productsSelected = [];
+    },
+    formatAmount(amount) {
+      return new Intl.NumberFormat().format(amount)
     },
     getTallas(product) {
       const tallas = [];
@@ -1020,12 +1063,12 @@ export default {
           item.product.regular_price ||
           item.product.variations[0].regular_price;
         const productTotal = price * item.quantity;
-        const totalFormat = new Intl.NumberFormat().format(productTotal);
+        const totalFormat = this.formatAmount(productTotal);
         total += productTotal;
         message += `\n${item.product.name} | Talla: ${tallas} - ${totalFormat}`;
       }
 
-      message += `\n\nTotal: ${new Intl.NumberFormat().format(total)}`;
+      message += `\n\nTotal: ${this.formatAmount(total)}`;
 
       let url = `https://api.whatsapp.com/send?text=${encodeURIComponent(
         message
