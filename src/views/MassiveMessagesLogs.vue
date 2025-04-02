ed07901dc686b8b30f21d3e90f818269b09e30dd
ViktorJJF <vj.jimenez96@gmail.com>
2025-04-01 20:12:15 -0500

    Update: adjusted pagination settings and added company ID to recent chats query in MassiveMessages and MassiveMessagesLogs components

--- src/views/MassiveMessagesLogs.vue
+++ src/views/MassiveMessagesLogs.vue
@@ -377,6 +377,7 @@ export default {
         this.$store.dispatch(ENTITY + "Module/list", {
           sort: "createdAt",
           order: -1,
+          limit: 3,
           companies: [
             this.$store.getters["authModule/getCurrentCompany"].company._id,
           ],
